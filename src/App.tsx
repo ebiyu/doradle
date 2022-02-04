fd654e5205deea39381a3f43c2778e3ed633f786
Ebihara Yusuke <me@ebiyuu.com>
2022-02-05 02:43:54 +0900

    feat: 基本的なゲームシステムを作製

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import pieList from "./pieList";
 
 function App() {
+  const pietype = [
+    ...Array.from("mps").flatMap((suit) =>
+      Array.from("123456789").map((num) => num + suit)
+    ),
+    ...Array.from("1234567").map((num) => num + "z"),
+  ];
+
+  const [ans, _setAns] = useState<string>(
+    pietype[Math.floor(Math.random() * pietype.length)]
+  );
+  const [list, setList] = useState<string[]>([]);
+
   return (
     <div className="App">
+      <h1 style={{ textAlign: "center" }}>Doradle</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {list.map((pie, i) => (
+          <div className="wanpai" key={i}>
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+            <div className={"answer" + (pie === ans ? " rightAnswer" : "")}>
+              <img src={pieList[pie]} alt={pie} />
+            </div>
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+          </div>
+        ))}
+        {!list.includes(ans) && (
+          <div className="wanpai">
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+            <div className="answer emptyAnswer"></div>
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+            <div className="answer ura"></div>
+          </div>
+        )}
+        <div className="buttons">
+          {pietype.map((pie) => (
+            <button
+              key={pie}
+              onClick={() => {
+                if (list.includes(ans)) return;
+                setList([...list, pie]);
+              }}
+              style={{
+                backgroundColor: list.includes(pie)
+                  ? pie === ans
+                    ? "green"
+                    : "gray"
+                  : "",
+              }}
+            >
+              <img src={pieList[pie]} alt={pie} />
+            </button>
+          ))}
+        </div>
       </header>
     </div>
   );
---
+++ src/pieList.ts
@@ -0,0 +1,73 @@
+import m1 from "./img/pie/manzu2_1/p_ms1_1.gif";
+import m2 from "./img/pie/manzu2_1/p_ms2_1.gif";
+import m3 from "./img/pie/manzu2_1/p_ms3_1.gif";
+import m4 from "./img/pie/manzu2_1/p_ms4_1.gif";
+import m5 from "./img/pie/manzu2_1/p_ms5_1.gif";
+import m6 from "./img/pie/manzu2_1/p_ms6_1.gif";
+import m7 from "./img/pie/manzu2_1/p_ms7_1.gif";
+import m8 from "./img/pie/manzu2_1/p_ms8_1.gif";
+import m9 from "./img/pie/manzu2_1/p_ms9_1.gif";
+import p1 from "./img/pie/pinzu2_1/p_ps1_1.gif";
+import p2 from "./img/pie/pinzu2_1/p_ps2_1.gif";
+import p3 from "./img/pie/pinzu2_1/p_ps3_1.gif";
+import p4 from "./img/pie/pinzu2_1/p_ps4_1.gif";
+import p5 from "./img/pie/pinzu2_1/p_ps5_1.gif";
+import p6 from "./img/pie/pinzu2_1/p_ps6_1.gif";
+import p7 from "./img/pie/pinzu2_1/p_ps7_1.gif";
+import p8 from "./img/pie/pinzu2_1/p_ps8_1.gif";
+import p9 from "./img/pie/pinzu2_1/p_ps9_1.gif";
+import s1 from "./img/pie/sozu2_1/p_ss1_1.gif";
+import s2 from "./img/pie/sozu2_1/p_ss2_1.gif";
+import s3 from "./img/pie/sozu2_1/p_ss3_1.gif";
+import s4 from "./img/pie/sozu2_1/p_ss4_1.gif";
+import s5 from "./img/pie/sozu2_1/p_ss5_1.gif";
+import s6 from "./img/pie/sozu2_1/p_ss6_1.gif";
+import s7 from "./img/pie/sozu2_1/p_ss7_1.gif";
+import s8 from "./img/pie/sozu2_1/p_ss8_1.gif";
+import s9 from "./img/pie/sozu2_1/p_ss9_1.gif";
+import z1 from "./img/pie/tupai2_1/p_ji_e_1.gif";
+import z2 from "./img/pie/tupai2_1/p_ji_s_1.gif";
+import z3 from "./img/pie/tupai2_1/p_ji_w_1.gif";
+import z4 from "./img/pie/tupai2_1/p_ji_n_1.gif";
+import z5 from "./img/pie/tupai2_1/p_no_1.gif";
+import z6 from "./img/pie/tupai2_1/p_ji_h_1.gif";
+import z7 from "./img/pie/tupai2_1/p_ji_c_1.gif";
+
+const pieList = {
+  "1m": m1,
+  "2m": m2,
+  "3m": m3,
+  "4m": m4,
+  "5m": m5,
+  "6m": m6,
+  "7m": m7,
+  "8m": m8,
+  "9m": m9,
+  "1p": p1,
+  "2p": p2,
+  "3p": p3,
+  "4p": p4,
+  "5p": p5,
+  "6p": p6,
+  "7p": p7,
+  "8p": p8,
+  "9p": p9,
+  "1s": s1,
+  "2s": s2,
+  "3s": s3,
+  "4s": s4,
+  "5s": s5,
+  "6s": s6,
+  "7s": s7,
+  "8s": s8,
+  "9s": s9,
+  "1z": z1,
+  "2z": z2,
+  "3z": z3,
+  "4z": z4,
+  "5z": z5,
+  "6z": z6,
+  "7z": z7,
+};
+
+export default pieList as { [key: string]: string };
